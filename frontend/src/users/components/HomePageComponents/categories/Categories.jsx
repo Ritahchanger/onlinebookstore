963d712aea50f21c7009c304fa988a3d0bf0974a
Dennis Peter <peterdennis573@gmail.com>
2024-05-09 10:28:55 +0300

    Updates on the categories carousel

---
+++ frontend/src/users/components/HomePageComponents/categories/Categories.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Slider from "react-slick";
+import "./Categories.css";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { CategoriesData } from "../../Data/CategoriesData";
+
+const Categories = () => {
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 700,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
+  return (
+    <div className="categories">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <p className="small-header">Categories</p>
+            <div className="medium-header">Explore our Top Categories</div>
+          </div>
+          <div className="col">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+              ex, sit deserunt adipisci corrupti molestias excepturi, corporis
+              nesciunt modi omnis odit quidem. Nulla molestiae, ducimus soluta
+              aliquid, nostrum nobis magnam aliquam, animi placeat maiores
+              dignissimos a perspiciatis sit quaerat porro!
+            </p>
+          </div>
+        </div>
+
+        <div className="books-categories">
+          <Slider {...settings}>
+            {CategoriesData.map((item, index) => (
+              <div className="book-show">
+                <div className="img-wrapper">
+                  <img src={item.imgUrl} alt="" />
+                </div>
+                <div className="card-body">
+                  <p className="small-header">{item.smallHeader}</p>
+                  <p className="description">{item.description}</p>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
