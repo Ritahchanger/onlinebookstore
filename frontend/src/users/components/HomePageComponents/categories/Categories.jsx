64e8fffb000a22852c72018857c3861707225c66
Dennis Peter <peterdennis573@gmail.com>
2024-05-09 11:08:34 +0300

    Checked react slick and carousel conflicts

--- frontend/src/users/components/HomePageComponents/categories/Categories.jsx
+++ frontend/src/users/components/HomePageComponents/categories/Categories.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
 import "./Categories.css";
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import { CategoriesData } from "../../Data/CategoriesData";
 
