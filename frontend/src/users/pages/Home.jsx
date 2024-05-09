02ae85da33d60a01d423675aae00a18a6e6d4db8
Dennis Peter <peterdennis573@gmail.com>
2024-05-09 16:59:56 +0300

    Commented some stuff

--- frontend/src/users/pages/Home.jsx
+++ frontend/src/users/pages/Home.jsx
@@ -10,9 +10,9 @@ const Home = () => {
     <Fragment>
       <Hero />
       <Categories />
-      <Ebook />
+      {/* <Ebook />
       <NewRelease />
-      <FeaturedBook/>
+      <FeaturedBook/> */}
     </Fragment>
   );
 };
