53e886405937be0b1023bbbc68457dbd36816f47
Dennis Peter <peterdennis573@gmail.com>
2024-05-09 17:03:01 +0300

    Uncommented ebook

--- frontend/src/users/pages/Home.jsx
+++ frontend/src/users/pages/Home.jsx
@@ -10,8 +10,8 @@ const Home = () => {
     <Fragment>
       <Hero />
       <Categories />
-      {/* <Ebook />
-      <NewRelease />
+      <Ebook />
+      {/* <NewRelease />
       <FeaturedBook/> */}
     </Fragment>
   );
