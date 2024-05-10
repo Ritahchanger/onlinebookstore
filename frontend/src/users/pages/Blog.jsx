8d1b31277bf9038eea95143f4d9cac9e1b174003
Dennis Peter <peterdennis573@gmail.com>
2024-05-10 17:58:07 +0300

    Updates on blog page

--- frontend/src/users/pages/Blog.jsx
+++ frontend/src/users/pages/Blog.jsx
@@ -10,6 +10,29 @@ const Blog = () => {
       <LowerNavbar />
       <div className="blog">
         <div className="container">
+          <p className="small-header">Latest Blogs</p>
+          <div className="blog-grid">
+            {blogSampleData.map((item, index) => (
+              <div className="blog-card">
+                <div className="img-wrapper">
+                  <img src={item.imgUrl} alt="" />
+                </div>
+                <div className="overlay">
+                  <p className="small-header">{item.blogTitle}</p>
+                  <p className="description">{item.description}</p>
+                  <div className="cardfooter">
+                    <span className="card_dates">{item.cardDates}</span>
+                    <span className="blog-btn">
+                      <a href="#">
+                        MORE <i class="fa fa-arrow-right"></i>{" "}
+                      </a>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <p className="small-header">Earlier Blogs</p>
           <div className="blog-grid">
             {blogSampleData.map((item, index) => (
               <div className="blog-card">
