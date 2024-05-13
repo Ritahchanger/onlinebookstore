e51ab879672c4f36e21758c874a9ab89a775fdd4
Dennis Peter <peterdennis573@gmail.com>
2024-05-13 15:21:24 +0300

    Single page book displayer

--- frontend/src/users/components/Books/BookByAuthor.jsx
+++ frontend/src/users/components/Books/BookByAuthor.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import LowerNavbar from "../LowerNavbar/LowerNavbar";
 import "./BooksByAuthor.css";
 import { books } from "../Data/BookData";
+import FeaturedBook from "../../../assets/images/cover13.jpg";
 
 const BookByAuthor = () => {
   return (
@@ -34,6 +35,34 @@ const BookByAuthor = () => {
           ))}
         </div>
         <p className="small-header">Ebooks</p>
+      </div>
+      <div className="showBookModal">
+        <div className="container">
+             <a href="#" className="close-item">&times;</a>
+          <div className="row">
+            <div className="col">
+              <div className="img-wrapper">
+                <img src={FeaturedBook} alt="" />
+              </div>
+            </div>
+            <div className="col">
+              <p className="small-header">The return of the maasai's</p>
+              <p className="description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
+                nulla maiores iure quasi, laboriosam delectus animi
+                reprehenderit quibusdam impedit et quia ad repellat ratione
+                perspiciatis quidem eaque sed in consequuntur libero, vero
+                distinctio explicabo quo? Iure suscipit, vitae voluptas labore
+                assumenda beatae unde tempore excepturi. Accusamus deserunt id
+                saepe tenetur odio aspernatur culpa soluta maiores reprehenderit
+                ullam, qui corporis voluptate?
+              </p>
+              <a href="#" className="hero-btn">
+                ADD TO CART
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
