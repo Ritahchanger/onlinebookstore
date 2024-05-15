7f15987ed2d75afe03e293a4080eba59f67e82d5
Dennis Peter <peterdennis573@gmail.com>
2024-05-15 12:11:08 +0300

    Updates on the single blog page

--- frontend/src/users/pages/SingleBlog.jsx
+++ frontend/src/users/pages/SingleBlog.jsx
@@ -19,53 +19,74 @@ const SingleBlog = () => {
       <div className="single_blog">
         <div className="container">
           <div className="blog-post">
-            <p className="small-header">MIAMI LIFE</p>
-            <div className="img-wrapper">
-              <img
-                src="https://images.unsplash.com/photo-1440151050977-247552660a3b?q=80&w=1390&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt=""
-              />
-            </div>
             <div className="blog-post-body">
-              <p className="description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-                nesciunt laborum quod quae nostrum ipsam quisquam placeat atque
-                totam commodi inventore quaerat accusantium maiores consectetur
-                eveniet alias tempore, veritatis consequatur, impedit optio.
-                Quas, cupiditate perferendis! Ipsa ducimus hic nemo sequi at
-                doloremque sint? Dicta magni cum rerum aut doloribus voluptates
-                facere soluta, assumenda at, reiciendis perferendis quod maiores
-                odio! Expedita, nobis similique quia sint facere itaque
-                dignissimos recusandae at fuga?
-              </p>
-              <div className="img-wrapper">
-                <img
-                  src="https://images.unsplash.com/photo-1578878703120-42d9fb8fd0af?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  alt=""
-                />
+              <p className="small-header">MIAMI LIFE</p>
+              <div className="description-body">
+                <p className="description">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Veniam nesciunt laborum quod quae nostrum ipsam quisquam
+                  placeat atque totam commodi inventore quaerat accusantium
+                  maiores consectetur eveniet alias tempore, veritatis
+                  consequatur, impedit optio. Quas, cupiditate perferendis! Ipsa
+                  ducimus hic nemo sequi at doloremque sint? Dicta magni cum
+                  rerum aut doloribus voluptates facere soluta, assumenda at,
+                  reiciendis perferendis quod maiores odio! Expedita, nobis
+                  similique quia sint facere itaque dignissimos recusandae at
+                  fuga?
+                  <img
+                    src="https://images.unsplash.com/photo-1578878703120-42d9fb8fd0af?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt=""
+                  />
+                </p>
+                <p className="description">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Veniam nesciunt laborum quod quae nostrum ipsam quisquam
+                  placeat atque totam commodi inventore quaerat accusantium
+                  maiores consectetur eveniet alias tempore, veritatis
+                  consequatur, impedit optio. Quas, cupiditate perferendis! Ipsa
+                  ducimus hic nemo sequi at doloremque sint? Dicta magni cum
+                  rerum aut doloribus voluptates facere soluta, assumenda at,
+                  reiciendis perferendis quod maiores odio! Expedita, nobis
+                  similique quia sint facere itaque dignissimos recusandae at
+                  fuga?
+                  <img
+                    src="https://plus.unsplash.com/premium_photo-1694475405897-c359309feeec?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt=""
+                  />
+                </p>
+                <p className="description">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Veniam nesciunt laborum quod quae nostrum ipsam quisquam
+                  placeat atque totam commodi inventore quaerat accusantium
+                  maiores consectetur eveniet alias tempore, veritatis
+                  consequatur, impedit optio. Quas, cupiditate perferendis! Ipsa
+                  ducimus hic nemo sequi at doloremque sint? Dicta magni cum
+                  rerum aut doloribus voluptates facere soluta, assumenda at,
+                  reiciendis perferendis quod maiores odio! Expedita, nobis
+                  similique quia sint facere itaque dignissimos recusandae at
+                  fuga?
+                  <img
+                    src="https://images.unsplash.com/photo-1613961314852-2182add356a0?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt=""
+                  />
+                </p>
+                <p className="description">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Veniam nesciunt laborum quod quae nostrum ipsam quisquam
+                  placeat atque totam commodi inventore quaerat accusantium
+                  maiores consectetur eveniet alias tempore, veritatis
+                  consequatur, impedit optio. Quas, cupiditate perferendis! Ipsa
+                  ducimus hic nemo sequi at doloremque sint? Dicta magni cum
+                  rerum aut doloribus voluptates facere soluta, assumenda at,
+                  reiciendis perferendis quod maiores odio! Expedita, nobis
+                  similique quia sint facere itaque dignissimos recusandae at
+                  fuga?
+                  <img
+                    src="https://images.unsplash.com/photo-1578878703120-42d9fb8fd0af?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt=""
+                  />
+                </p>
               </div>
-              <p className="description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-                nesciunt laborum quod quae nostrum ipsam quisquam placeat atque
-                totam commodi inventore quaerat accusantium maiores consectetur
-                eveniet alias tempore, veritatis consequatur, impedit optio.
-                Quas, cupiditate perferendis! Ipsa ducimus hic nemo sequi at
-                doloremque sint? Dicta magni cum rerum aut doloribus voluptates
-                facere soluta, assumenda at, reiciendis perferendis quod maiores
-                odio! Expedita, nobis similique quia sint facere itaque
-                dignissimos recusandae at fuga?
-              </p>
-              <p className="description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-                nesciunt laborum quod quae nostrum ipsam quisquam placeat atque
-                totam commodi inventore quaerat accusantium maiores consectetur
-                eveniet alias tempore, veritatis consequatur, impedit optio.
-                Quas, cupiditate perferendis! Ipsa ducimus hic nemo sequi at
-                doloremque sint? Dicta magni cum rerum aut doloribus voluptates
-                facere soluta, assumenda at, reiciendis perferendis quod maiores
-                odio! Expedita, nobis similique quia sint facere itaque
-                dignissimos recusandae at fuga?
-              </p>
               <p className="date">6/12/2024</p>
             </div>
           </div>
