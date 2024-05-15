7513ee80d74d6d2faefd116e9d80ff81202b39ec
Dennis Peter <peterdennis573@gmail.com>
2024-05-15 13:31:12 +0300

    Updates

--- frontend/src/users/pages/SingleBlog.jsx
+++ frontend/src/users/pages/SingleBlog.jsx
@@ -1,32 +1,9 @@
-import LowerNavbar from "../components/LowerNavbar/LowerNavbar";
-
-import { Fragment } from "react";
-
-import { blogSampleData } from "../components/Data/BlogData";
-
-import BlogItem from "../components/BlogPageComponents/BlogItem";
-
-import Footer from "../components/Footer/Footer";
-
-import "./single-page.css";
+import React from 'react'
 
 const SingleBlog = () => {
   return (
-    <Fragment>
-      <LowerNavbar />
-      <div className="single_blog">
-        <div className="container">
-          <p className="small-header">Related Blogs</p>
-          <div className="grid">
-            {blogSampleData.map((item, index) => (
-              <BlogItem item={item} key={index} />
-            ))}
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </Fragment>
-  );
-};
+    <div>SingleBlog</div>
+  )
+}
 
-export default SingleBlog;
+export default SingleBlog
