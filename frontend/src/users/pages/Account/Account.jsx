de67f08c696ad32d6b0ecfa3d7e39cf726d24c9f
Dennis Peter <peterdennis573@gmail.com>
2024-05-23 18:35:40 +0300

    Updates

--- frontend/src/users/pages/Account/Account.jsx
+++ frontend/src/users/pages/Account/Account.jsx
@@ -286,6 +286,32 @@ const Account = () => {
           </table>
         </div>
       </div>
+
+      <div className="terminate-account-modal">
+        <form>
+          <a  href="#" className="close-modal">&times;</a>
+          <p className="medium-header">ACCOUNT TERMINATION</p>
+          <div className="input-group">
+            <input
+              type="email"
+              name="email"
+              id=""
+              placeholder="Enter your email.."
+            />
+          </div>
+          <div className="input-group">
+            <input
+              type="password"
+              name="password"
+              id=""
+              placeholder="Enter your password.."
+            />
+          </div>
+          <div className="input-group">
+            <input type="submit" value="REQUEST TERMINATION" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
